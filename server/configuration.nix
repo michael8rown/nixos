389c6981780ccc88bb2fff8c2f4ffcaf1062246e
Michael Brown <michael8rown@gmail.com>
2024-05-12 11:50:37 -0500

    updated server config

--- server/configuration.nix
+++ server/configuration.nix
@@ -127,7 +127,7 @@ in
   #  system.autoUpgrade.enable = true;
   #  system.autoUpgrade.allowReboot = false;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos-server"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.interfaces.enp4s0.ipv4.addresses = [ { address = "10.11.12.122"; prefixLength = 24; } ];
   # networking.defaultGateway = "10.11.12.1";
