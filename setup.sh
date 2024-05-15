96fea959ea70544932f8c9791b6487dd8009e185
Michael Brown <michael8rown@gmail.com>
2024-05-15 06:17:46 -0500

    updated setup

--- setup.sh
+++ setup.sh
@@ -4,19 +4,19 @@ set -e -o pipefail
 declare -A arr
 
 arr+=(
-["VAR_USERNAME"]=username 			# your actual username
-["VAR_YOUR_NAME"]="Firstname Lastname"		# your first and last name, such as John Smith
-["VAR_MSMTP_EMAIL"]=username@yourdomain.com	# the email address that should be used for `msmtp`
-["VAR_MSMTP_SERVER"]=mail.yourdomain.com	# the mailserver address, such as `mail.yourdomain.com`
-["VAR_EMAIL"]=personalemail@gmail.com		# your personal email address, such as `johnsmith@qooqle.com`
-["VAR_SSH_PORT"]=####				# the port you use for `sshd`
-["VAR_HTTP_ROOT"]=/http/root			# the root directory of your webserver, e.g., `/var/www` or `/srv/httpd`
-["VAR_HOSTNAME"]=hostname			# the name of your machine
+  ["VAR_USERNAME"]=username 			# your actual username
+  ["VAR_YOUR_NAME"]="Firstname Lastname"	# your first and last name, such as John Smith
+  ["VAR_MSMTP_EMAIL"]=username@yourdomain.com	# the email address that should be used for `msmtp`
+  ["VAR_MSMTP_SERVER"]=mail.yourdomain.com	# the mailserver address, such as `mail.yourdomain.com`
+  ["VAR_EMAIL"]=personalemail@gmail.com		# your personal email address, such as `johnsmith@qooqle.com`
+  ["VAR_SSH_PORT"]=####				# the port you use for `sshd`
+  ["VAR_HTTP_ROOT"]=/http/root			# the root directory of your webserver, e.g., `/var/www` or `/srv/httpd`
+  ["VAR_HOSTNAME"]=hostname			# the name of your machine
 )
 
 echo "The following configurations are available:"
 echo
-find . -type d | grep -E "(.){3,}" | sed -E "s/^.\///g"
+find . -type d | grep -E "^./[^.](.){2,}" | sed -E "s/^.\///g"
 echo
 read -p "Which one would you like to setup? " configuration
 
