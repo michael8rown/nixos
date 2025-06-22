4692994c942f35cd0dd1e0606e9de1b8801c9130
Michael Brown <michael8rown@gmail.com>
2025-06-21 20:29:45 -0600

    Updated timezone

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -89,7 +89,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/Chicago";
+  time.timeZone = "America/Denver";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
